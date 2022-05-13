92a9972fd236d05f0dab1d53a2bfe863f3b1685c
NaraSyuhei <besumero628@gmail.com>
2022-05-13 23:09:07 +0900

    [Add] setting login root

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './components/pages/Home';
+import { Login } from './components/pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
