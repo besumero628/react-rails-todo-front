32ca3ca3754af1dd5424676641f41849f259684e
NaraSyuhei <besumero628@gmail.com>
2022-05-28 15:12:08 +0900

    [Add] footer.tsx

--- src/components/templates/HomeLayout.tsx
+++ src/components/templates/HomeLayout.tsx
@@ -1,9 +1,11 @@
 import { FC, memo } from "react";
+import { Foooter } from "../organisms/layout/Footer";
 
 export const HomeLayout: FC = memo(()=> {
     return (
         <>
             <p>home layout</p>
+            <Foooter />
         </>
     )
 })
