38f129b08837dc724d05c4e9207eee2082d90310
NaraSyuhei <besumero628@gmail.com>
2022-05-29 20:55:21 +0900

    [Add] Header layout

--- src/components/templates/HomeLayout.tsx
+++ src/components/templates/HomeLayout.tsx
@@ -1,9 +1,11 @@
 import { FC, memo } from "react";
 import { Foooter } from "../organisms/layout/Footer";
+import { Header } from "../organisms/layout/Header";
 
 export const HomeLayout: FC = memo(()=> {
     return (
         <>
+            <Header />
             <p>home layout</p>
             <Foooter />
         </>
