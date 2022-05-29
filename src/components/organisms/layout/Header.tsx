4b3ec28d743ce6abf33b0166aafd78b0525b4811
NaraSyuhei <besumero628@gmail.com>
2022-05-29 20:54:20 +0900

    [Add] header flex

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,8 +1,18 @@
+import { Flex } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 export const Header: FC = memo(()=> {
     return (
         <>
+            <Flex
+                flexDirection="column"
+                width="100wh"
+                minH="10vh"
+                backgroundColor="gray.200"
+                justifyContent="center"
+                alignItems="center"
+                // position="absolute"
+            ></Flex>
         </>
     )
 })
