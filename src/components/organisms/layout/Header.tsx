e752ef657338b414f762f32caa9dec7c6b1967e3
NaraSyuhei <besumero628@gmail.com>
2022-06-01 23:27:09 +0900

    [add] buttongroup

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,5 +1,6 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { ButtonGroup, Flex, Heading, Spacer } from "@chakra-ui/react";
 import { FC, memo } from "react";
+import { PrimaryButton } from "../../atom/button/PrimaryButton";
 
 export const Header: FC = memo(()=> {
     return (
@@ -10,12 +11,17 @@ export const Header: FC = memo(()=> {
                 minH="10vh"
                 backgroundColor="gray.200"
                 justifyContent="center"
-                alignItems="center"
+                alignItems="left"
             >
                 <Flex align="center" as="a" mr={8} _hover={{cursor: "pointer"}} >
                     <Heading as="h1" fontSize={{base: "md", md:"lg"}}>
-                        ユーザー管理アプリ
+                        React×Rails ToDO APP
                     </Heading>
+                    <Spacer />
+                    <ButtonGroup gap='2'>
+                        <PrimaryButton>sign in</PrimaryButton>
+                        <PrimaryButton>Logout</PrimaryButton>
+                    </ButtonGroup>
                 </Flex>
             </Flex>
         </>
