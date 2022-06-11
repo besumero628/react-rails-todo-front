dabccf2c0ec8498ebc3e3ca61a1f7ed441d1b039
NaraSyuhei <besumero628@gmail.com>
2022-06-11 23:41:44 +0900

    [Add] 404 notfound

--- src/components/templates/Page404Layout.tsx
+++ src/components/templates/Page404Layout.tsx
@@ -1,3 +1,4 @@
+import { Container, Stack, Box, Heading, Text } from "@chakra-ui/react";
 import { FC, memo } from "react";
 import { Header } from "../organisms/layout/Header";
 
@@ -5,7 +6,25 @@ export const Page404Layout: FC = memo(() => {
     return(
         <>
             <Header />
-                <p>test</p>
+                <Container maxW={'3xl'}>
+                    <Stack
+                        as={Box}
+                        textAlign={'center'}
+                        spacing={{ base: 8, md: 14 }}
+                        py={{ base: 20, md: 36 }}
+                    >
+                    <Heading
+                        fontWeight={600}
+                        fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+                        lineHeight={'110%'}>
+                        <Text as={'span'} color={'green.400'}>
+                        404<br />
+                        </Text>
+                        Not Found
+                    </Heading>
+                    </Stack>
+                    
+                </Container>
         </>
     )
 })
